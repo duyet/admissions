3e760159441206bc4f588c616eb2d86609226eb9
Eroshaly <eroshaly@gmail.com>
2015-08-11 17:36:25 +0700

    Modify Front-End: Add loading icon

--- public/application.js
+++ public/application.js
@@ -18,3 +18,43 @@ angular.element(document).ready(function() {
 	//Then init the app
 	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
 });
+
+var loading_button = {
+	load_icon : '<span class="loader"><svg class="circular">'+
+					    '<circle class="path" cx="16" cy="16" r="8" fill="none" stroke-width="2" stroke-miterlimit="10"/>'+
+					  '</svg></span>',
+	load_success : "<span class='glyphicon glyphicon-ok'></span>",
+	load_error : "<span class='glyphicon glyphicon-remove'></span>",
+	loading : function (button) {
+		var button_html = document.getElementById(button);
+		button_html.className = "btn btn-loading";
+		button_html.innerHTML = this.load_icon;
+	},
+	success : function(button) {
+		var button_html = document.getElementById(button);
+		button_html.className = "btn btn-success"; 
+		button_html.innerHTML = this.load_success;
+	},
+	error :function (button) {
+		var button_html = document.getElementById(button);
+		button_html.className = "btn btn-danger";
+		button_html.innerHTML = this.load_error;
+	},
+}
+var loading_page = {
+	load_icon : '<div id="loader-page" class="loader-page"><svg class="circular">'+
+					    '<circle class="path" cx="32" cy="32" r="16" fill="none" stroke-width="3" stroke-miterlimit="10"/>'+
+					  '</svg></div>',
+	init : function () {
+		document.body.innerHTML = document.body.innerHTML + this.load_icon;
+	},
+	loading : function () {
+		var button_html = document.getElementById('loader-page');
+		button_html.className = "loader-page show";
+	},
+	hide : function() {
+		var button_html = document.getElementById('loader-page');
+		button_html.className = "loader-page hide";
+	},
+}
+loading_page.init();
